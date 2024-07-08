57060518da96e51f7b70631573e9d42dbdf70f81
Strone <sangbbang31@gmail.com>
2024-07-08 15:54:49 +0900

    Dispatcher Injection 추가

---
+++ app/src/main/java/com/strone/bitwin/di/CoroutineDispatchersModule.kt
@@ -0,0 +1,33 @@
+package com.strone.bitwin.di
+
+import com.strone.core.qualifier.DefaultDispatcher
+import com.strone.core.qualifier.IoDispatcher
+import com.strone.core.qualifier.MainDispatcher
+import com.strone.core.qualifier.MainImmediateDispatcher
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+
+@InstallIn(SingletonComponent::class)
+@Module
+object CoroutineDispatchersModule {
+
+    @DefaultDispatcher
+    @Provides
+    fun providesDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
+
+    @IoDispatcher
+    @Provides
+    fun providesIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
+
+    @MainDispatcher
+    @Provides
+    fun providesMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
+
+    @MainImmediateDispatcher
+    @Provides
+    fun providesMainImmediateDispatcher(): CoroutineDispatcher = Dispatchers.Main.immediate
+}
