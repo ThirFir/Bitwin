8b3186979d3d3ea2734eb051552352ac6ebdd6d3
Strone <sangbbang31@gmail.com>
2024-07-08 17:14:38 +0900

    KRW Market만 수집하도록 filtering

--- domain/src/main/java/com/strone/domain/usecase/FetchTickerUseCase.kt
+++ domain/src/main/java/com/strone/domain/usecase/FetchTickerUseCase.kt
@@ -1,9 +1,11 @@
 package com.strone.domain.usecase
 
 import com.strone.domain.model.Market
+import com.strone.domain.model.MarketType
 import com.strone.domain.model.Ticker
 import com.strone.domain.repository.TickerRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.filter
 import javax.inject.Inject
 
 class FetchTickerUseCase @Inject constructor(
@@ -12,7 +14,9 @@ class FetchTickerUseCase @Inject constructor(
 
     suspend operator fun invoke(markets: List<Market>) : Result<Flow<Ticker>> {
         return try {
-            Result.success(tickerRepository.fetchTickerResponse(markets.map(Market::code)))
+            Result.success(tickerRepository.fetchTickerResponse(markets.map(Market::code)).filter {
+                it.type == MarketType.KRW
+            })
         } catch (e: Exception) {
             Result.failure(e)
         }
