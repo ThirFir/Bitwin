d925165b78860e3e330830bf5d1dc114be560fbb
Strone <sangbbang31@gmail.com>
2024-07-08 16:52:12 +0900

    코인 이름 관리 object 추가, app 시작점에서 fetch

--- app/src/main/java/com/strone/bitwin/BitwinApplication.kt
+++ app/src/main/java/com/strone/bitwin/BitwinApplication.kt
@@ -1,7 +1,36 @@
 package com.strone.bitwin
 
 import android.app.Application
+import com.strone.core.CryptoNamespace
+import com.strone.core.qualifier.IoDispatcher
+import com.strone.data.api.rest.MarketApi
+import com.strone.data.mapper.toMarket
 import dagger.hilt.android.HiltAndroidApp
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 @HiltAndroidApp
-class BitwinApplication : Application()
+class BitwinApplication : Application() {
+
+    @Inject
+    @IoDispatcher
+    lateinit var ioDispatcher: CoroutineDispatcher
+
+    @Inject
+    lateinit var marketApi: MarketApi
+
+    override fun onCreate() {
+        super.onCreate()
+        initializeCryptoNamespace()
+    }
+
+    @OptIn(DelicateCoroutinesApi::class)
+    private fun initializeCryptoNamespace() {
+        GlobalScope.launch(ioDispatcher) {
+            CryptoNamespace.putMarkets(marketApi.fetchAllMarkets().map { it.toMarket() })
+        }
+    }
+}
