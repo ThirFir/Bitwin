dd9b5f5dd4f70dda9274a0a19f695d1d670599a7
Strone <sangbbang31@gmail.com>
2024-07-24 18:19:13 +0900

    Serializable 확장함수 추가

--- presentation/src/main/java/com/strone/presentation/util/AndroidExtensions.kt
+++ presentation/src/main/java/com/strone/presentation/util/AndroidExtensions.kt
@@ -3,12 +3,23 @@ package com.strone.presentation.util
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import android.content.Intent
+import android.os.Build
+import java.io.Serializable
 
-fun Context.findActivity(): Activity {
+internal fun Context.findActivity(): Activity {
     var context = this
     while (context is ContextWrapper) {
         if (context is Activity) return context
         context = context.baseContext
     }
     throw IllegalStateException("no activity")
+}
+
+internal inline fun<reified T: Serializable> Intent.getSerializableExtraCompat(key: String): T? {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+        getSerializableExtra(key, T::class.java)
+    } else {
+        getSerializableExtra(key) as T?
+    }
 }
