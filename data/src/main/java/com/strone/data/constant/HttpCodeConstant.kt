fca40284a978761689783678279bbf3f24e13ac5
Strone <sangbbang31@gmail.com>
2024-08-03 15:04:42 +0900

    Http Exception 코드, 메시지 추가

---
+++ data/src/main/java/com/strone/data/constant/HttpCodeConstant.kt
@@ -0,0 +1,14 @@
+package com.strone.data.constant
+
+object HttpCodeConstant {
+    const val BAD_REQUEST = 400
+    const val UNAUTHORIZED = 401
+    const val FORBIDDEN = 403
+    const val NOT_FOUND = 404
+
+    const val INTERNAL_SERVER_ERROR = 500
+    const val NOT_IMPLEMENTED = 501
+    const val BAD_GATEWAY = 502
+    const val SERVICE_UNAVAILABLE = 503
+    const val GATEWAY_TIMEOUT = 504
+}
